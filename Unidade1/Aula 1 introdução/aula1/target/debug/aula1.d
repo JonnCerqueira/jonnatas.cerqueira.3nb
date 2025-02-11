8e6b2d0880526b83fef0bdc9bf5680bfa0da1ae9
JonnCerqueira <jonnatas.cerqueira@gmail.com>
2025-02-11 00:50:44 +0000

    A aula foi top

---
+++ Unidade1/Aula 1 introdução/aula1/target/debug/aula1.d
@@ -0,0 +1,1 @@
+/workspaces/jonnatas.cerqueira.3nb/Unidade1/Aula\ 1\ introdução/aula1/target/debug/aula1: /workspaces/jonnatas.cerqueira.3nb/Unidade1/Aula\ 1\ introdução/aula1/src/main.rs
---
+++ Unidade1/Aula 1 introdução/aula1/target/debug/deps/aula1-0feb40cd98cdc893.d
@@ -0,0 +1,5 @@
+/workspaces/jonnatas.cerqueira.3nb/Unidade1/Aula 1 introdução/aula1/target/debug/deps/aula1-0feb40cd98cdc893: src/main.rs
+
+/workspaces/jonnatas.cerqueira.3nb/Unidade1/Aula 1 introdução/aula1/target/debug/deps/aula1-0feb40cd98cdc893.d: src/main.rs
+
+src/main.rs:
